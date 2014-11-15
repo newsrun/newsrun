53be790ba2c69f9158bcb8331948feba996571c7
Yiqing Zhu <sbbird.zhu@gmail.com>
2014-11-16 05:36:31 +0900

    Added analyzer helper. Usage: http://localhost:9292/test_analysis

--- app/routes/news.rb
+++ app/routes/news.rb
@@ -3,13 +3,21 @@
 
 class GenApp < Sinatra::Base
 
-  get '/test' do
+  get '/test_create' do
     News.delete_all
     helper = News_Helper.new
     helper.create_recode
     @events = News.all
     erb :news_test
-
   end
 
+  get '/test_analysis' do 
+  	news = News.all
+  	helper = Analysis_Helper.new
+
+  	@keywords = news.map{ |n|
+  		helper.analyze_keywords(n.Title)
+  	}	
+  	erb :analysis_test
+  end
 end
