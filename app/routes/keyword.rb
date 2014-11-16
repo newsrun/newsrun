5aa3ada1be125562ea83eea07b9dc162a773eb12
Yiqing Zhu <sbbird.zhu@gmail.com>
2014-11-16 10:47:16 +0900

    Revised default value of keyword

--- app/routes/keyword.rb
+++ app/routes/keyword.rb
@@ -5,7 +5,7 @@ class GenApp < Sinatra::Base
 
   get '/test_keyword' do 
    	kh = Keyword_Helper.new
-    @keywords = kh.get_keywords(5)
+    @keywords = kh.get_keywords(15)
     erb :keyword_test
   end
 end
